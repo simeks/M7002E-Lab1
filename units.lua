33d4a50f237f5063a22aabfd7db49de091b36abc
Simon <simon.ekstrom@gmail.com>
2014-01-21 07:28:11 +0100

    Tundra configuration for OSX.

--- units.lua
+++ units.lua
@@ -8,6 +8,7 @@ StaticLibrary {
 			".",
 			"dependencies/SDL2-2.0.1/include",
 			"dependencies/glew/include",
+			{ "/Library/Frameworks/SDL2.framework/Headers"; Config = "macosx-*-*" }
 		}, 
 	},
 
@@ -21,6 +22,8 @@ StaticLibrary {
 			},
 		},
 	},
+
+	Frameworks = { "OpenGL", "SDL2" },
 }
 
 Program {
@@ -32,8 +35,11 @@ Program {
 			"dependencies/glew/include",
 		}, 
 		LIBPATH = {
-			"dependencies/SDL2-2.0.1/lib/x86",
-			"dependencies/glew/lib/Release/Win32",
+			{
+				"dependencies/SDL2-2.0.1/lib/x86",
+				"dependencies/glew/lib/Release/Win32";
+				Config = "win32-*-*";
+			}
 		},
 		PROGOPTS = {
 			{ "/SUBSYSTEM:WINDOWS"; Config = { "win32-*-*" } },
@@ -51,20 +57,21 @@ Program {
 	},
 	Depends = { "Framework" },
 
-	Libs = 
+	Libs = { 
 		{ 
-			{ 
-				"kernel32.lib", 
-				"user32.lib", 
-				"gdi32.lib", 
-				"comdlg32.lib", 
-				"advapi32.lib", 
-				"SDL2.lib",
-				"opengl32.lib",
-				"glew32.lib",
-				Config = { "win32-*-*", "win64-*-*" } 
-			}
-		},
+			"kernel32.lib", 
+			"user32.lib", 
+			"gdi32.lib", 
+			"comdlg32.lib", 
+			"advapi32.lib", 
+			"SDL2.lib",
+			"opengl32.lib",
+			"glew32.lib",
+			Config = { "win32-*-*", "win64-*-*" } 
+		}
+	},
+
+	Frameworks = { "OpenGL", "SDL2" },
 }
 
 Default "Lab1"
